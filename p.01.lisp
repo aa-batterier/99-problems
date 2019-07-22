e716809899d4e1465c1cd23d719ba5a427728f77
Andreas Johansson <aj854@outlook.com>
2019-07-22 15:10:01 +0000

    Improved the recursive function my-last-v3.

--- p.01.lisp
+++ p.01.lisp
@@ -1,17 +1,21 @@
 ; Problem 1.
 ; Find the last box of a list.
+
+; Simple solution.
 (defun my-last (l)
   (last l))
 
+; Little bit more advance solution.
 (defun my-last-v2 (l)
   (if l
     (list (first (reverse l)))))
 
+; Recursive solution.
 (defun my-last-v3 (l)
-  (cond ((null l) nil)
-	((null (cdr l)) (list (first l)))
+  (cond ((null (cdr l)) l)
 	(t (my-last-v3 (cdr l)))))
 
+; Macro solution.
 (defmacro my-last-macro (l)
   `(if ,l
      (list (first (reverse ,l)))))
